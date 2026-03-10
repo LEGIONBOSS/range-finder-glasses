ce1a11249b10e5eae2e39a950c2782bc4110d436
Szilágyi Dániel <17363065+LEGIONBOSS@users.noreply.github.com>
2026-03-10 20:39:49 +0100

    Change sonar sensor library

--- range-finder-glasses.ino
+++ range-finder-glasses.ino
@@ -1,4 +1,4 @@
-#include "src/HCSR04/HCSR04.h" // https://github.com/d03n3rfr1tz3/HC-SR04
+#include "src/HCSR04/HCSR04.h"                             // https://github.com/gamegine/HCSR04-ultrasonic-sensor-lib
 #include "src/ShiftRegister74HC595/ShiftRegister74HC595.h" // https://github.com/Simsso/ShiftRegister74HC595
 
 void setup(void)
