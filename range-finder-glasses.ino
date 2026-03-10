f8d3269f5c01cda6e4fc26100f9184509571801a
Dániel Szilágyi <szilagyi.daniel88@gmail.com>
2026-03-10 10:24:12 +0100

    Add libraries

---
+++ range-finder-glasses.ino
@@ -0,0 +1,12 @@
+#include "src/HCSR04/HCSR04.h" // https://github.com/d03n3rfr1tz3/HC-SR04
+#include "src/ShiftRegister74HC595/ShiftRegister74HC595.h" // https://github.com/Simsso/ShiftRegister74HC595
+
+void setup(void)
+{
+    /*setup code*/
+}
+
+void loop(void)
+{
+    /*looping code*/
+}
